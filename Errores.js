651308b978ae4965264bf172744fed5c5be78d25
erickcampos <erickcampos865@gmail.com>
2024-10-23 23:18:47 -0600

    errores java semana 1

---
+++ Errores.js
@@ -0,0 +1,24 @@
+
+var nombre= 'juan';
+var apellido='campos'
+function imprimirNombre(){
+console.log(nombre);
+var apellido = 'munoz'
+console.log(apellido);
+}
+imprimirNombre();
+console.log(apellido);
+
+
+const edad= 30;
+altura = 30;
+
+
+for (let i = 0; i< 5; i++) {
+setTimeout(function() {
+console.log(i);
+}, 1000);
+}
+
+let producto = 'leche';
+let producto2 = 'pan';
