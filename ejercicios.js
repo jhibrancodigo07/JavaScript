8c1aa31c7debda93f0906ae8030bf5ca71cd4f20
erickcampos <erickcampos865@gmail.com>
2024-10-23 20:22:37 -0600

    primera parte

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,27 @@
 function numarg(a, b) {
     
     if (!isFinite(a) || !isFinite(b) || isNaN(a) || isNaN(b)) {
+        return true;
+    }else
+    {
+       return false;
+    }
+    
+   
+   
+}
+
+console.log(numarg(NaN, 10));
+console.log(numarg(Infinity, 10));
+console.log(numarg(5, 10));    
+
+
+
+
+
+function numarg(a, b) {
+    
+    if (!isFinite(a) || !isFinite(b) || isNaN(a) || isNaN(b)) {
         return 0;
     }else
     {
@@ -16,3 +37,15 @@ console.log(numarg(5, 10));
 //console.log(numarg(Infinity,10)); 
 
 
+ function numarg(a, b){
+        
+    if (a === 0) {
+    return true;
+} else {
+    return false;
+}
+
+
+}
+console.log(numarg(10, 2));  
+console.log(numarg(10, 0));
